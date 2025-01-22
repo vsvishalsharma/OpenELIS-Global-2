ef1a72a0deb386efa94da2ffcb7f78efd2389fb0
mozzy11 <mozzymutesa@gmail.com>
2025-01-22 10:38:45 +0300

    turn off cypress video recording to optimise running e2e tests

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   defaultCommandTimeout: 8000,
   viewportWidth: 1200,
   viewportHeight: 700,
+  video: false,
   watchForFileChanges: false,
   e2e: {
     setupNodeEvents(on, config) {
