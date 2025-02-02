f6523f58e1764f78f1e509089ae86ed9437521e4
Agaba Derrick <agabaderrick18@gmail.com>
2025-02-02 12:46:34 +0300

    Add datasets

--- src/test/java/org/openelisglobal/referencetables/ReferenceTablesServiceTest.java
+++ src/test/java/org/openelisglobal/referencetables/ReferenceTablesServiceTest.java
@@ -1,10 +1,13 @@
 package org.openelisglobal.referencetables;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openelisglobal.BaseWebContextSensitiveTest;
@@ -20,36 +23,30 @@ public class ReferenceTablesServiceTest extends BaseWebContextSensitiveTest {
 
     @Before
     public void init() throws Exception {
+        executeDataSetWithStateManagement("testdata/referencetables.xml");
     }
 
     @After
     public void tearDown() {
+
     }
 
     @Test
     public void getReferenceTableByName_shouldReturnCorrectReferenceTable() throws Exception {
         String tableName = "TestTable";
-
-        ReferenceTables refTable = createReferenceTable(tableName);
-        referenceTablesService.insert(refTable);
-
         ReferenceTables retrievedTable = referenceTablesService.getReferenceTableByName(tableName);
         assertNotNull(retrievedTable);
-        assertEquals(tableName, retrievedTable.getTableName());
+        assertEquals(tableName, retrievedTable.getName());
     }
 
     @Test
     public void duplicateReferenceTable_shouldThrowException() throws Exception {
         String tableName = "DuplicateTable";
-
-        ReferenceTables refTable1 = createReferenceTable(tableName);
-        referenceTablesService.insert(refTable1);
-
-        ReferenceTables refTable2 = createReferenceTable(tableName);
+        ReferenceTables refTable = createReferenceTable(tableName);
 
         try {
-            referenceTablesService.insert(refTable2);
-            Assert.fail("Expected LIMSDuplicateRecordException to be thrown");
+            referenceTablesService.insert(refTable);
+            fail("Expected LIMSDuplicateRecordException to be thrown");
         } catch (LIMSDuplicateRecordException e) {
             assertEquals("Duplicate record exists for " + tableName, e.getMessage());
         }
@@ -65,52 +62,30 @@ public class ReferenceTablesServiceTest extends BaseWebContextSensitiveTest {
 
     @Test
     public void insertReferenceTable_shouldInsertCorrectly() throws Exception {
-        ReferenceTables refTable = createReferenceTable("InsertTestTable");
+        ReferenceTables refTable = createReferenceTable("NewInsertTestTable");
         referenceTablesService.insert(refTable);
-        ReferenceTables retrievedTable = referenceTablesService.getReferenceTableByName("InsertTestTable");
+        ReferenceTables retrievedTable = referenceTablesService.getReferenceTableByName("NewInsertTestTable");
         assertNotNull(retrievedTable);
-        assertEquals("InsertTestTable", retrievedTable.getTableName());
+        assertEquals("NewInsertTestTable", retrievedTable.getTableName());
     }
 
     @Test
     public void getAllReferenceTables_shouldReturnAllTables() throws Exception {
-        int initialCount = referenceTablesService.getAllReferenceTables().size();
-
-        ReferenceTables refTable1 = createReferenceTable("Table1");
-        ReferenceTables refTable2 = createReferenceTable("Table2");
-        referenceTablesService.insert(refTable1);
-        referenceTablesService.insert(refTable2);
-
         List<ReferenceTables> allTables = referenceTablesService.getAllReferenceTables();
-        assertEquals(initialCount + 2, allTables.size());
+        assertTrue(allTables.size() >= 6);
     }
 
     @Test
     public void getTotalReferenceTableCount_shouldReturnCorrectCount() throws Exception {
-        int initialCount = referenceTablesService.getTotalReferenceTableCount();
-
-        ReferenceTables refTable1 = createReferenceTable("CountTable1");
-        ReferenceTables refTable2 = createReferenceTable("CountTable2");
-        referenceTablesService.insert(refTable1);
-        referenceTablesService.insert(refTable2);
-
-        int newCount = referenceTablesService.getTotalReferenceTableCount();
-        assertEquals(initialCount + 2, newCount);
+        int count = referenceTablesService.getTotalReferenceTableCount();
+        assertTrue(count >= 8);
     }
 
     @Test
     public void getAllReferenceTablesForHl7Encoding_shouldReturnOnlyHl7EncodedTables() throws Exception {
-        ReferenceTables hl7Table = createReferenceTable("Hl7Table");
-        hl7Table.setIsHl7Encoded("Y");
-        referenceTablesService.insert(hl7Table);
-
-        ReferenceTables nonHl7Table = createReferenceTable("NonHl7Table");
-        nonHl7Table.setIsHl7Encoded("N");
-        referenceTablesService.insert(nonHl7Table);
-
         List<ReferenceTables> hl7EncodedTables = referenceTablesService.getAllReferenceTablesForHl7Encoding();
         assertTrue(hl7EncodedTables.stream().allMatch(t -> "Y".equals(t.getIsHl7Encoded())));
-        assertTrue(hl7EncodedTables.stream().anyMatch(t -> "Hl7Table".equals(t.getTableName())));
-        Assert.assertFalse(hl7EncodedTables.stream().anyMatch(t -> "NonHl7Table".equals(t.getTableName())));
+        assertTrue(hl7EncodedTables.stream().anyMatch(t -> "Hl7Table".equals(t.getName())));
+        assertFalse(hl7EncodedTables.stream().anyMatch(t -> "NonHl7Table".equals(t.getName())));
     }
 }
